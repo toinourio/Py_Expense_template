31f941496b53f6a5270a6d1246e566e3a615f88b
Antoine Rio <antoine.rio@epita.fr>
2023-09-20 21:43:29 +0200

    expenses from each persons gotten

--- status.py
+++ status.py
@@ -13,7 +13,11 @@ def get_expenses():
 def show_status():
     expenses = get_expenses()
     users = get_users()[1]
+    accounts = {}
     for user in users:
-        print(user, expenses[0][-1])
+        accounts[user] = 0
+    for expense in expenses:
+        accounts[expense[2]] += int(expense[0])
+    print(accounts)
 
     return True
